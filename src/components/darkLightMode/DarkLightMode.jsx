1905a2b7bbfba508c6b0b493196872759d1db5f8
NiamorDev <niamordev@proton.me>
2022-12-12 12:58:34 +0100

    update V2

--- src/components/darkLightMode/DarkLightMode.jsx
+++ src/components/darkLightMode/DarkLightMode.jsx
@@ -2,32 +2,6 @@ import React, { useEffect, useState } from 'react'
 import UseLocalStorage from '../../hooks/UseLocalStorage'
 export default function DarkLightMode() {
 
-    // const [theme, setTheme] = useState('light-theme')
-    
-    // const toggleTheme = () => {
-    //         if(theme === 'light-theme' ){
-    //         localStorage.setItem('aa', 'dark-theme')
-            
-    //         }
-    //     else {
-    //         localStorage.setItem('aa', 'light-theme')
-    // }
-    // const data = localStorage.getItem('aa');
-    // setTheme(data)
-
-    // }
-    
-    // useEffect(() => {
-    //     const data = localStorage.getItem('aa');
-    //     setTheme(data)
-    // }, [theme]);
-
-
-    // useEffect(() => {
-    //     document.body.className = theme
-
-    // },[theme]);
-
 
 
 
@@ -61,8 +35,8 @@ export default function DarkLightMode() {
 
     return(
         <div onClick={toggleTheme} className="dark-light-theme">
-            <img className='dark-mode-element' src="../assets/img/moon-dark-mode-white.svg" alt="" />
-            <img className='light-mode-element' src="../assets/img/moon-dark-mode.svg" alt="" />
+            <img className='dark-mode-element' src="../my-portfolio/assets/img/moon-dark-mode-white.svg" alt="" />
+            <img className='light-mode-element' src="../my-portfolio/assets/img/moon-dark-mode.svg" alt="" />
             <span>Dark Mode</span>
         </div>
     )
