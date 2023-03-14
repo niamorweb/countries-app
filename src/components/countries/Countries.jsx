137a93b5149449672b76637d6c5364a145fac022
NiamorDev <122058097+niamorweb@users.noreply.github.com>
2023-03-14 13:40:47 +0100

    update

--- src/components/countries/Countries.jsx
+++ src/components/countries/Countries.jsx
@@ -2,86 +2,85 @@ import React, { useEffect, useState } from "react";
 import axios from "axios";
 import CardCountries from "../cardCountries/CardCountries";
 
-export default function Countries( {dataCountries}) {
-
-
+export default function Countries({ dataCountries }) {
   const countriesToFilter = ["Africa", "America", "Europe", "Oceania", "Asia"];
   const [selectedOption, setSelectedOption] = useState("");
   const [selectedOptionSort, setSelectedOptionSort] = useState("name");
   const [searchValue, setSearchValue] = useState("");
 
-
   return (
     <div className="countries">
-      <ul className="radio-container">
-        <li>
-          <input
-            onChange={(e) => setSearchValue(e.target.value)}
-            placeholder="Search for a country..."
-            type="search"
-            name=""
-            id="search-country"
-          />
-        </li>
-
-        <div className="div-selectors">
+      <div className="countries__container">
+        <ul className="radio-container">
           <li>
-            <select
-              onChange={(e) => setSelectedOption(e.target.value)}
-              name="selector-region"
-              id="selector-region"
-            >
-              <option value="">Filter by Region</option>
-              {countriesToFilter.map((continent) => (
-                <option value={continent}>{continent} </option>
-              ))}
-            </select>
-          </li>
-
-          <li>
-            <select
-              onChange={(e) => setSelectedOptionSort(e.target.value)}
-              name="selector-sort"
-              id="selector-sort"
-            >
-              <option value={"name"}>Sort by country name</option>
-              <option value={"croissant"}>
-                Sort by increasing population{" "}
-              </option>
-              <option value={"decroissant"}>
-                Sort by decreasing population{" "}
-              </option>
-            </select>
-          </li>
-        </div>
-      </ul>
-
-      <ul id="countries-main">
-        {dataCountries
-          .filter((country) => country.continents[0].includes(selectedOption))
-          .filter((country) =>
-            country.name.common
-              .toLowerCase()
-              .includes(searchValue.toLowerCase())
-          )
-          .sort((a, b) => a.name.common.localeCompare(b.name.common))
-          .sort(
-            selectedOptionSort === "name"
-              ? (a, b) => a.name.common.localeCompare(b.name.common)
-              : selectedOptionSort === "decroissant"
-              ? (a, b) => b.population - a.population
-              : selectedOptionSort === "croissant"
-              ? (a, b) => a.population - b.population
-              : null
-          )
-          .map((country, index) => (
-            <CardCountries
-              key={index}
-              country={country}
-              dataCountries={dataCountries}
+            <input
+              onChange={(e) => setSearchValue(e.target.value)}
+              placeholder="Search for a country..."
+              type="search"
+              name=""
+              id="search-country"
             />
-          ))}
-      </ul>
+          </li>
+
+          <div className="div-selectors">
+            <li>
+              <select
+                onChange={(e) => setSelectedOption(e.target.value)}
+                name="selector-region"
+                id="selector-region"
+              >
+                <option value="">Filter by Region</option>
+                {countriesToFilter.map((continent) => (
+                  <option value={continent}>{continent} </option>
+                ))}
+              </select>
+            </li>
+
+            <li>
+              <select
+                onChange={(e) => setSelectedOptionSort(e.target.value)}
+                name="selector-sort"
+                id="selector-sort"
+              >
+                <option value={"name"}>Sort by country name</option>
+                <option value={"croissant"}>
+                  Sort by increasing population{" "}
+                </option>
+                <option value={"decroissant"}>
+                  Sort by decreasing population{" "}
+                </option>
+              </select>
+            </li>
+          </div>
+        </ul>
+
+        <ul id="countries-main">
+          {dataCountries
+            .filter((country) => country.continents[0].includes(selectedOption))
+            .filter((country) =>
+              country.name.common
+                .toLowerCase()
+                .includes(searchValue.toLowerCase())
+            )
+            .sort((a, b) => a.name.common.localeCompare(b.name.common))
+            .sort(
+              selectedOptionSort === "name"
+                ? (a, b) => a.name.common.localeCompare(b.name.common)
+                : selectedOptionSort === "decroissant"
+                ? (a, b) => b.population - a.population
+                : selectedOptionSort === "croissant"
+                ? (a, b) => a.population - b.population
+                : null
+            )
+            .map((country, index) => (
+              <CardCountries
+                key={index}
+                country={country}
+                dataCountries={dataCountries}
+              />
+            ))}
+        </ul>
+      </div>
     </div>
   );
 }
--- src/components/darkLightMode/DarkLightMode.jsx
+++ src/components/darkLightMode/DarkLightMode.jsx
@@ -1,43 +1,41 @@
-import React, { useEffect, useState } from 'react'
-import UseLocalStorage from '../../hooks/UseLocalStorage'
+import React, { useEffect, useState } from "react";
+import UseLocalStorage from "../../hooks/UseLocalStorage";
 export default function DarkLightMode() {
+  const [theme, setTheme] = UseLocalStorage("setupColorsTheme", "light-theme");
 
-
-
-
-    const [theme, setTheme] = UseLocalStorage('setupColorsTheme', 'light-theme')
-    
-    const toggleTheme = () => {
-            if(theme === 'light-theme' ){
-            setTheme('dark-theme')
-            
-            }
-        else {
-            setTheme('light-theme')
+  const toggleTheme = () => {
+    if (theme === "light-theme") {
+      setTheme("dark-theme");
+    } else {
+      setTheme("light-theme");
     }
+  };
 
-    }
-    
+  useEffect(() => {
+    document.body.className = theme;
+  });
 
-    useEffect(() => {
-        document.body.className = theme
-    })
+  useEffect(() => {
+    setTheme(theme);
+  }, [theme]);
 
-    useEffect(() => {
-        setTheme(theme)
-    }, [theme]);
+  useEffect(() => {
+    document.body.className = theme;
+  }, [theme]);
 
-
-    useEffect(() => {
-        document.body.className = theme
-
-    },[theme]);
-
-    return(
-        <div onClick={toggleTheme} className="dark-light-theme">
-            <img className='dark-mode-element' src="../countries-app/assets/img/moon-dark-mode-white.svg" alt="" />
-            <img className='light-mode-element' src="../countries-app/assets/img/moon-dark-mode.svg" alt="" />
-            <span>Dark Mode</span>
-        </div>
-    )
+  return (
+    <div onClick={toggleTheme} className="dark-light-theme">
+      <img
+        className="dark-mode-element"
+        src="/countries-app/assets/img/moon-dark-mode-white.svg"
+        alt=""
+      />
+      <img
+        className="light-mode-element"
+        src="/countries-app/assets/img/moon-dark-mode.svg"
+        alt=""
+      />
+      <span>Dark Mode</span>
+    </div>
+  );
 }
